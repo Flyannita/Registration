63f3de0d531dce4f1892e486438ecbe6613736f9
Flyna <evtyakovavika@gmail.com>
2024-12-03 18:25:50 +0300

    Update regest.js

--- regest.js
+++ regest.js
@@ -4,7 +4,6 @@ function chemail(idshka,formochka) {
   e = /[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}/
   n = i.match(e)
   d = document.getElementById(idshka)
-  console.log(n)
   if (n == null) {
 
     d.style.color = "red"
@@ -24,7 +23,7 @@ function chepass(formochka, idshka) {
   o = /[A-Z]+[a-z]+[0-9]+/
   n = j.match(o)
   h = document.getElementById(idshka)
-  console.log(n)
+
   if (n == null) {
 		
     h.style.color = "red"
@@ -45,11 +44,7 @@ r = new FormData(f)
 i = r.get("mail")
 j = r.get("pass")
 if (l && o) {
-https = "https://server-kege100.onrender.com/get_data?key=" + i 
-get_response = await fetch(https)
-dataget = await get_response.json()
-if (dataget.error){
-post_response = await fetch("https://server-kege100.onrender.com/set_data", { 
+post_response = await fetch("http://127.0.0.1:5000/registration", { 
 method: "POST",
 headers: {"Content-Type": "application/json"},
 body: JSON.stringify({ key: i, value: j})
@@ -63,19 +58,13 @@ v.style.display = "none"
 e.style.display = "block"
 }
 else{
-console.error("ошибка при регистрации")
+console.error("почта существует")
+enr()
 }
 }
 else{
 console.error("ошибка сети")
 }
-}
-else{
-alert("Такая почта уже существует, переводим вас на страницу входа")
-enr()
-}
-
-
 }
 }
 
@@ -83,12 +72,12 @@ enr()
 async function enter(){
 f2 = document.getElementById("fmr")
 l = chepass(f2,"pas")
-o = chemail("emai",f2)
+o = chemail("mail",f2)
 r = new FormData(f2)
-i = r.get("mail")
+i = r.get("emai")
 j = r.get("pass")
 if (l && o) {
-https = "https://server-kege100.onrender.com/get_data?key=" + i 
+https = "http://127.0.0.1:5000/enter"
 get_response = await fetch(https)
 dataget = await get_response.json()
 if (dataget.error){
@@ -107,6 +96,42 @@ alert("Почта или пароль не совпадают")
 }
 }
 }
+
+
+async function NewPasswords(){
+forma = document.getElementById("forgotForm")
+CheckPass = chepass(forma,"NewPassword")
+CheckMail = chemail("emailfrfrgt",forma)
+NewForm = new FormData(forma)
+Maile = NewForm.get("mail")
+PassGet = NewForm.get("pass")
+PassReap = NewForm.get("passreap")
+if (PassGet == PassReap && CheckMail && CheckPass){
+console.log(PassGet, PassReap)
+https = "http://127.0.0.1:5000/forgot"
+post_response = await fetch(https,{ 
+method: "POST",
+headers: {"Content-Type": "application/json"},
+body: JSON.stringify({ key: Maile, value: PassGet})
+})
+if (post_response.ok){
+dataget = await post_response.json()
+console.log(dataget)
+if (dataget.error){
+alert(dataget.error)
+}
+else{
+alert("Успешно сменен пароль, переводим вас на страницу входа")
+exitForgot()
+}
+}
+else{
+alert("Почта или пароль не совпадают")
+}
+}
+}
+
+
 
 function enr(){
 v = document.getElementById("enter")
@@ -128,4 +153,16 @@ e = document.getElementById("lololoshka")
 e.style.display = "none"
 v.style.display = "block"
 }
- 
+
+function ForgotPasswordAgain(){
+v = document.getElementById("enter")
+e = document.getElementById("ForgotPassword")
+v.style.display = "none"
+e.style.display = "block"
+}
+function exitForgot(){
+v = document.getElementById("enter")
+e = document.getElementById("ForgotPassword")
+e.style.display = "none"
+v.style.display = "block"
+}
